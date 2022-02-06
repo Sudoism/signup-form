62b43749526363a8e15d30e5606eb628e6337cc9
Alexander Hed <alexander.hed@gmail.com>
2022-02-06 18:17:24 +0100

    some final tweaks

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const form = document.getElementById("account");
 form.noValidate=true;
 const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirm-password");
+const passwordError = document.getElementById("password-error");
 
 form.addEventListener('submit', validateForm);
 
 function validateForm(e) {
-
     if (password.value === confirmPassword.value) {
         console.log("success")
         password.classList.remove("invalid");
         confirmPassword.classList.remove("invalid");
+        passwordError.innerHTML = '';
     } else {
         console.log(password.value)
         console.log(confirmPassword.value)
@@ -18,15 +19,6 @@ function validateForm(e) {
         console.log("not same value!")
         password.classList.add("invalid");
         confirmPassword.classList.add("invalid");
+        passwordError.innerHTML = '* Passwords do not match';
     }
 }
-
-
-//form.addEventListener("submit", (e) => {
-//    if (password.value != confirmPassword.value) {
-//        console.log("not same value!")
-//        password.setAttribute("class", "invalid");
-//    } else {
-//        return;
-//    }
-//})
